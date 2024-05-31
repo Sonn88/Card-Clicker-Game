424810d85174397a7b3c5868650eefcd5eee9b8d
5ezd <5ezd@fresnou.org>
2024-05-31 14:20:26 -0700

    Need to add points

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const cardList= document.querySelector(`.cardList`)
-buildBoard();
+const score = document.createElement('p')
+const scoreboard= document.getElementById('sBoard');
+const allpoints = document.getElementById('game-over')
+const points= document.createElement('h1')
 
 function  addCard(value){
     let card = document.createElement('div')
@@ -13,12 +16,28 @@ function buildBoard(){
         addCard('starter')
     }
 }
+buildBoard();
+let interval = setInterval(function () {
+    addCard(cardList.children.length + 1)//make it start ar 1 not 0
+}, 2000);
+
 cardList.addEventListener('click',function (e){
     console.log(e.target);
     if(e.target.matches('.cardList')){
         return
     }
     if(e.target.classList.contains('active')){
-
+     e.target.classList.remove('active');
+     e.target.classList.add('inactive');
+     return
     }
+    e.target.remove();
+    let children = cardList.children;
+    if(children.length<1){
+        clearInterval(interval)
+      interval=null
+        points.innerHTML=`YOU BEAT THE GAME YOU GOT POINTS`
+        allpoints.appendChild(points)
+    }
+
 })
